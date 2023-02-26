cd0a14ca90cf20f71be006f5e3f026e09fc751de
Bohdan Zborovskyi <baga142536@gmail.com>
2023-02-26 22:34:09 +0100

    add rebase_2 m

--- src/Feature4.java
+++ src/Feature4.java
@@ -2,11 +2,12 @@ public class Feature4 {
 
     int feature_1;
     int feature_2;
-    int feature_3;
+    int rebase_m_2;
     int feature_m_1;
     int feature_m_2;
     int feature_m_3;
     int rebase_m_1;
+
 
 
 }
